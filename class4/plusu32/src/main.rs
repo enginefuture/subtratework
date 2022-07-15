fe95204967cad2419884a3de61f696816af3ba3f
enginefuture <tabshi@outlook.com>
2022-07-15 17:27:01 +0800

    master

---
+++ class4/plusu32/src/main.rs
@@ -0,0 +1,21 @@
+fn plusu32(s :&[u32]) -> Option<u32>{
+    let sum = s.iter().try_fold(0u32, |sum,x|sum.checked_add(*x));
+    return sum
+}
+fn main() {
+    let maxu32 = u32::MAX;
+    // print!("u32 max is {}------",maxu32);
+    let s = [maxu32,1];
+    print!("arr is {:?}",s);
+    // let sum = plusu32(&s);
+    match plusu32(&s) {
+        Some(x) => print!("-------sum is {}\n",x),
+        None =>print!("-------sum is None \n"),
+    }
+    let a = [19u32,1];
+    print!("arr is {:?}",a);
+    match plusu32(&a) {
+        Some(x) => print!("-------sum is {} \n",x),
+        None =>print!("-------sum is None \n"),
+    }
+}
